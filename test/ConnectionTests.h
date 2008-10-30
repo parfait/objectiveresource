684122efd5d6f356b0aa288d6881ddef11513c63
Ciarán Walsh <git@ciaranwal.sh>
2008-10-30 09:41:38 +0000

    Cleaning up the tests a little

--- test/ConnectionTests.h
+++ test/ConnectionTests.h
@@ -1,8 +1,10 @@
 #import <SenTestingKit/SenTestingKit.h>
+#import <ObjectiveResource/Connection.h>
 
 #define TEST_SITE_URL       @"http://caboose:monkeyballs@localhost:3000"
 
 @interface ConnectionTests : SenTestCase
 {
+	ORConnection* connection;
 }
 @end
--- test/ConnectionTests.mm
+++ test/ConnectionTests.mm
@@ -1,22 +1,34 @@
 #import "ConnectionTests.h"
-#import <ObjectiveResource/Connection.h>
 #define HC_SHORTHAND
 #import <hamcrest/hamcrest.h>
 
 @implementation ConnectionTests
+- (void)setUp
+{
+	connection = [[ORConnection alloc] initToSite:TEST_SITE_URL format:@"xml"];
+}
+
+- (void)tearDown
+{
+	[connection release];
+}
+
 - (void)testConnection
 {
-	ORConnection* connection = [[[ORConnection alloc] initToSite:TEST_SITE_URL format:@"xml"] autorelease];
-	id result                = [connection get:@"products/1.xml" headers:nil];
+	id result = [connection get:@"products/1.xml" headers:nil];
 	assertThat(result, instanceOf([NSDictionary class]));
 	STAssertTrue([result count] > 0, @"Result should have elements");
 }
 
 - (void)testConnectionWithArrayResult
 {
-	ORConnection* connection = [[[ORConnection alloc] initToSite:TEST_SITE_URL format:@"xml"] autorelease];
-	id result                = [connection get:@"products.xml" headers:nil];
+	id result = [connection get:@"products.xml" headers:nil];
 	assertThat(result, instanceOf([NSArray class]));
 	STAssertTrue([result count] > 0, @"Result should have elements");
+}
+
+- (void)testConnectionWith404Result
+{
+	STAssertThrows([connection get:@"foobarbaz.xml" headers:nil], @"ORConnection should throw on 404");
 }
 @end
