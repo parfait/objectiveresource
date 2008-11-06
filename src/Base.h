8bbaf127a77f9793da0ec216325dbe27cec41110
Ciarán Walsh <git@ciaranwal.sh>
2008-11-06 23:26:58 +0000

    Adding a few comments to the ObjectiveResource interface

--- src/Base.h
+++ src/Base.h
@@ -18,14 +18,22 @@ enum ORAttributeType
 + (NSString*)baseURL; // NO trailing slash!
 + (NSString*)user;
 + (NSString*)password;
+
+// The singular name for the resource in lowercase. Defaults to the class name.
 + (NSString*)elementName;
+
+// The plural name for the resource. Defaults to the plural form of +elementName
 + (NSString*)collectionName;
 
 + (id)find:(int)recordId;
 + (id)findAll:(id)options;
 
-- (NSString*)title;
-- (ORAttributeType)typeForAttribute:(NSString*)attributeName;
-
 - (void)reload;
+
+// The title for the record.
+// Checks for a 'title' attribute first, and then tries a 'name' attribute.
+- (NSString*)title;
+
+// Subclass to override the type for attributes.
+- (ORAttributeType)typeForAttribute:(NSString*)attributeName;
 @end
